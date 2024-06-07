eeabea108a2d258d7eb3ea3bee9220abfdf60f30
Tamanna Khan <ktamanna06@gmail.com>
2024-06-07 00:21:07 -0400

    MyProject

---
+++ src/test/java/MavenProject/Example.java
@@ -0,0 +1,20 @@
+package MavenProject;
+
+import org.testng.annotations.Test;
+
+public class Example {
+	
+	@Test
+    public void addSum() {
+        
+        System.out.println("Sum");
+    }
+    
+    @Test
+    public void lastClass() {
+        
+        System.out.println("Good bye!!!");
+    }
+    
+}
+
